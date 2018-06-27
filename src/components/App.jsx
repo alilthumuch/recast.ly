a27ee7adf1577782f15bc45f54c3631ad75d1a17
Hack Reactor Student <student@hackreactor.com>
2018-06-27 16:44:41 -0700

    Completed functionality for VideoList, VideoListEntry, and VideoPlayer

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,10 +7,10 @@ var App = () => (
     </nav>
     <div className="row">
       <div className="col-md-7">
-        <div><h5><em>videoPlayer</em> view goes here</h5></div>
+        <VideoPlayer video={window.exampleVideoData[0]}/>
       </div>
       <div className="col-md-5">
-        <div><h5><em>videoList</em> view goes here</h5></div>
+        <VideoList videos={window.exampleVideoData}/>
       </div>
     </div>
   </div>
@@ -19,3 +19,8 @@ var App = () => (
 // In the ES6 spec, files are "modules" and do not share a top-level scope
 // `var` declarations will only exist globally where explicitly defined
 window.App = App;
+
+
+//line 13 <div><h5><em>videoList</em> view goes here</h5></div> 
+
+//line 10 <div><h5><em>videoPlayer</em> view goes here</h5></div>
